c321e114dcf9fc88dc4d854a45000d4b6a5f7931
SophieIsCuteGirl <122952220+SophieIsCuteGirl@users.noreply.github.com>
2023-01-17 21:31:54 -0600

    Add files via upload

---
+++ Assets/frameWork/scriptableObjects/recipe.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Tool", menuName = "RPG/recipe")]
+public class recipe : ScriptableObject
+{
+	public item[] inputItems;
+	public item[] outputItems;
+}
