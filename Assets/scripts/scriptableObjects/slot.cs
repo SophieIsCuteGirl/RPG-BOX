40aff84125ced6ea17d0bd289a5347ef39d8f94c
SophieIsCuteGirl <122952220+SophieIsCuteGirl@users.noreply.github.com>
2023-01-22 09:07:25 -0600

    test ig

---
+++ Assets/scripts/scriptableObjects/slot.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "slot", menuName = "RPG-Box/slot")]
+public class slot : ScriptableObject
+{
+	public item item;
+}
