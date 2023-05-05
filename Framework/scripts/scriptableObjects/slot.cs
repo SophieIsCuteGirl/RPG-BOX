94afbc6f9b06e86272f92170cc1aff4893a27842
SophieIsCuteGirl <122952220+SophieIsCuteGirl@users.noreply.github.com>
2023-05-05 00:18:54 -0500

    Delete Framework/scripts directory

--- Framework/scripts/scriptableObjects/slot.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "slot", menuName = "RPG-Box/slot")]
-public class slot : ScriptableObject
-{
-	public item item;
-}
